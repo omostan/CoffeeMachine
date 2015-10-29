44ec7ced35ad292fc26e6c1705091b02f3adca36
omostan <stan@omotech.com>
2015-10-29 03:36:34 +0100

    bug fix

--- CoffeeMachine.Model/StateMachine/CoffeeMachineModel.cs
+++ CoffeeMachine.Model/StateMachine/CoffeeMachineModel.cs
@@ -10,119 +10,7 @@ namespace CoffeeMachine.Model.StateMachine
 
     using Stateless;
 
-    //public sealed class CoffeeMachineModel : StateMachine<CoffeeMachineState, CoffeeMachineTrigger>, INotifyPropertyChanged
-    //{
-    //	private const double CoffeePrice = 2;
-
-    //    private double _insertedMoney;
-    //	public double InsertedMoney
-    //	{
-    //		get
-    //		{
-    //			return this._insertedMoney;
-    //		}
-    //		private set
-    //		{
-    //			if (Math.Abs(this._insertedMoney) > 0)
-    //			{
-    //				return;
-    //			}
-    //			this._insertedMoney = value;
-    //			this.OnPropertyChanged("InsertedMoney");
-    //		}
-    //	}
-
-    //    private double _preparationProcess;
-    //       public double PreparationProcess
-    //	{
-    //		get
-    //		{
-    //			return this._preparationProcess;
-    //		}
-    //		private set
-    //		{
-    //			if (Math.Abs(this._preparationProcess - value) > 0)
-    //			{
-    //				return;
-    //			}
-    //			this._preparationProcess = value;
-    //			this.OnPropertyChanged("PreparationProcess");
-    //		}
-    //	}
-
-    //	public CoffeeMachineModel() : base(CoffeeMachineState.Idle)
-    //	{
-    //		this.ConfigureMachine();
-    //	}
-
-    //	private void ConfigureMachine()
-    //	{
-    //		this.Configure(CoffeeMachineState.Idle).Permit(CoffeeMachineTrigger.InsertMoney, CoffeeMachineState.WithMoney);
-    //		this.Configure(CoffeeMachineState.RefundMoney).OnEntry(this.RefundMoney).Permit(CoffeeMachineTrigger.MoneyRefunded, CoffeeMachineState.Idle);
-    //		this.Configure(CoffeeMachineState.WithMoney).PermitReentry(CoffeeMachineTrigger.InsertMoney).Permit(CoffeeMachineTrigger.RefundMoney, CoffeeMachineState.RefundMoney).Permit(CoffeeMachineTrigger.EnoughMoney, CoffeeMachineState.CanSelectCoffee);
-    //		this.Configure(CoffeeMachineState.CanSelectCoffee).PermitReentry(CoffeeMachineTrigger.InsertMoney).Permit(CoffeeMachineTrigger.RefundMoney, CoffeeMachineState.RefundMoney).Permit(CoffeeMachineTrigger.PrepareCoffee, CoffeeMachineState.PreparingCoffee);
-    //		this.Configure(CoffeeMachineState.PreparingCoffee).OnEntry(this.PrepareCoffee).Permit(CoffeeMachineTrigger.CoffeePrepared, CoffeeMachineState.CoffeeReady);
-    //		this.Configure(CoffeeMachineState.CoffeeReady).Permit(CoffeeMachineTrigger.TakeCoffe, CoffeeMachineState.RefundMoney);
-    //		this.OnTransitioned(this.NotifyStateChanged);
-    //	}
-
-    //	public void InsertCoin(double amount)
-    //	{
-    //		var insertedMoney = this;
-    //		insertedMoney.InsertedMoney = insertedMoney.InsertedMoney + amount;
-    //		if ((this.State == CoffeeMachineState.WithMoney && this.InsertedMoney >= 2))
-    //		{
-    //			this.Fire(CoffeeMachineTrigger.EnoughMoney);
-    //		}
-    //	}
-
-    //	private void NotifyStateChanged(Transition transition)
-    //	{
-    //		this.OnPropertyChanged("State");
-    //	}
-
-    //	[NotifyPropertyChangedInvocator]
-    //	private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    //	{
-    //		var handler = this.PropertyChanged;
-    //		if (handler != null)
-    //		{
-    //			handler(this, new PropertyChangedEventArgs(propertyName));
-    //		}
-    //	}
-
-    //	private void PrepareCoffee()
-    //	{
-    //		(new Task(() => {
-    //			this.InsertedMoney = this.InsertedMoney - 2;
-    //			while (this.PreparationProcess < 100)
-    //			{
-    //				Thread.Sleep(50);
-    //				var preparationProcess = this;
-    //				preparationProcess.PreparationProcess = preparationProcess.PreparationProcess + 1;
-    //			}
-    //			this.PreparationProcess = 0;
-    //			this.Fire(CoffeeMachineTrigger.CoffeePrepared);
-    //		})).Start();
-    //	}
-
-    //	private void RefundMoney()
-    //	{
-    //		(new Task(() => {
-    //			while (this.InsertedMoney > 1)
-    //			{
-    //				Thread.Sleep(200);
-    //				this.InsertedMoney = this.InsertedMoney - 1;
-    //			}
-    //			this.InsertedMoney = 0;
-    //			this.Fire(CoffeeMachineTrigger.MoneyRefunded);
-    //		})).Start();
-    //	}
-
-    //	public event PropertyChangedEventHandler PropertyChanged;
-    //}
-
-    public sealed class CoffeeMachineModel : StateMachine<CoffeeMachineState, CoffeeMachineTrigger>, INotifyPropertyChanged
+   public sealed class CoffeeMachineModel : StateMachine<CoffeeMachineState, CoffeeMachineTrigger>, INotifyPropertyChanged
     {
         private const double CoffeePrice = 2;
 
@@ -135,7 +23,7 @@ namespace CoffeeMachine.Model.StateMachine
             }
             private set
             {
-                if( this._InsertedMoney == value )
+                if( Equals(this._InsertedMoney, value) )
                 {
                     return;
                 }
@@ -153,7 +41,7 @@ namespace CoffeeMachine.Model.StateMachine
             }
             private set
             {
-                if( this._PreparationProcess == value )
+                if( Equals(this._PreparationProcess, value) )
                 {
                     return;
                 }
